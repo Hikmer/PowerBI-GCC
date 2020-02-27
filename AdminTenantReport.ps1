3af5b15a91e64ba1720ee956d8a6d3d536f819fa
Hikmer <cyruswc@gmail.com>
2020-02-27 13:34:07 -0800

    Add files via upload

---
+++ AdminTenantReport.ps1
@@ -0,0 +1,3 @@
+﻿Install-Module -Name MicrosoftPowerBIMgmt -Scope CurrentUser
+Connect-PowerBIServiceAccount -Environment USGov
+Invoke-PowerBIRestMethod -Url 'admin/groups?$top=5000&$skip=0&$expand=reports,dashboards,datasets,users,dataflows' -Method GET  | Out-File "C:\MyPathHere\PowerBIResults.json"
