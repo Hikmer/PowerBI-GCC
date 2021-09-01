dcb56d008f87de2ae3d1b698918c97bb9ff0d30e
Hikmer <cyruswc@gmail.com>
2021-09-01 15:51:37 -0700

    Add files via upload

---
+++ GatewayAdmin.ps1
@@ -0,0 +1,26 @@
+﻿Install-Module -Name DataGateway
+	 
+#Login to O365 as Admin: 
+ 
+Login-DataGatewayServiceAccount -Environment USGov
+
+#List existing gateways
+
+#https://docs.microsoft.com/en-us/powershell/module/datagateway/get-datagatewaycluster?view=datagateway-ps
+Get-DataGatewayCluster -Scope Organization
+ 
+#Consider restricting gateway setup moving forward
+#https://docs.microsoft.com/en-us/powershell/module/datagateway/set-datagatewaytenantpolicy?view=datagateway-ps
+Set-DataGatewayTenantPolicy -PersonalGatewayInstallPolicy Restricted
+Set-DataGatewayTenantPolicy -ResourceGatewayInstallPolicy Restricted
+
+#Reopen gaeway access for a brief period of time to install
+Set-DataGatewayTenantPolicy -PersonalGatewayInstallPolicy Open
+Set-DataGatewayTenantPolicy -ResourceGatewayInstallPolicy Open
+
+#Assign a user to allow gateway install rights
+#https://docs.microsoft.com/en-us/powershell/module/datagateway/set-datagatewayinstaller?view=datagateway-ps
+
+$user1 = $(Get-AzADUser -ObjectId "testUpn@tenant.com").Id
+$user2 = $(Get-AzADUser -ObjectId "testUpn@tenant.com").Id
+Set-DataGatewayInstaller -PrincipalObjectIds $user1,$user2 -Operation Add -GatewayType Resource
